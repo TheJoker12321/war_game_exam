a3754f81c077de6c6a53b174ab03fb9f54fec294
Idan <idansano10@gmail.com>
2025-10-23 09:46:26 +0300

    added create_card function

--- utils/deck.py
+++ utils/deck.py
@@ -1,5 +1,8 @@
 def create_card(rank:str,suite:str) -> dict:
-    pass
+    cards_values = {"2": 2, "3": 3,"4": 4, "5": 5, "6": 6, "7": 7,
+                    "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+    card_dictionary = {"rank": rank, "suite": suite, "value":cards_values[rank]}
+    return card_dictionary
 
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
     pass
