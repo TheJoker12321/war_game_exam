b01c2e27aa695e3aca30b7e4f929b239f2e4c2f4
Idan <idansano10@gmail.com>
2025-10-23 10:54:39 +0300

    added create_player function

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,8 +1,11 @@
-def create_player(name:str) -> dict:
-    pass
+def create_player(name:str = "AI") -> dict:
+    player_dictionary = {"name": name, "hand": [], "won_pile": []}
+    return player_dictionary
 
-def init_game()->dict:
+def init_game() -> dict:
     pass
 
 def play_round(p1:dict,p2:dict):
     pass
+
+print(create_player("Gal"))
